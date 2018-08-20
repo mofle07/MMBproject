38ef8ea65b40024fd3acc48d421f3cde32933b2b
zhoujiuzhou <35538853+zhoujiuzhou@users.noreply.github.com>
2018-08-20 09:34:18 +0800

    细节完善

--- js/brand.js
+++ js/brand.js
@@ -4,7 +4,8 @@ function getBrand() {
 
     // 获取地址栏中的参数
     var param =  getParam();
-
+    console.log(param);
+    
     $.ajax({
 
         url: "http://mmb.ittun.com/api/getbrand",
@@ -15,10 +16,15 @@ function getBrand() {
             // 因为传回来的数据已经是一个JSON对象,所以转换报错
             // var resObj = JSON.parse(res);
             //报错: Uncaught SyntaxError: Unexpected token o in JSON at position 1
+          
             
             var htmlStr = template("brandTmp", res);
+            
+            console.log(htmlStr);
             $('.brands').html(htmlStr);
-
+            console.log($('.brands'));
+            
+            console.log(res);
         }
     })
 
